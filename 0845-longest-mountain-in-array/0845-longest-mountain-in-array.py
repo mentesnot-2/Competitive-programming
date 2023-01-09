4936dd6441fa873bcc1febd1f0c79f829c1becca
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-01-09 05:12:48 -0800

    Time: 172 ms (88.67%), Space: 15.2 MB (39.04%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        lngstMtn=0
+        longestMountain=0
         for i in range(1,len(arr)-1):
             if arr[i-1]<arr[i] and arr[i+1]<arr[i]:
                 l=i-1
@@ -9,6 +9,5 @@ class Solution:
                     l-=1
                 while r<len(arr)-1 and arr[r+1]<arr[r]:
                     r+=1
-                lngstMtn=max(lngstMtn,r-l+1)
-        return lngstMtn
-                    
+                longestMountain=max(longestMountain,r-l+1)
+        return longestMountain
