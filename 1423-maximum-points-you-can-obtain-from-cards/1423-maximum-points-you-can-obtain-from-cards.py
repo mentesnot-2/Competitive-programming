53f8ea9186d6ed5a15bb2045731a07251b8814f3
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-01-09 03:19:31 -0800

    Time: 411 ms (98.56%), Space: 27.5 MB (14.67%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        best = score = sum(cardPoints[:k])
-        for i in range(1, k+1):
-            score = score - cardPoints[k-i] + cardPoints[-i]
-            best = score if score > best else best
-            
-        return best
+        total=sum(cardPoints[:k])
+        score=sum(cardPoints[:k])
+        for i in range(1,k+1):
+            score=score-cardPoints[k-i]+cardPoints[-i]
+            total=max(score,total)
+        return total
