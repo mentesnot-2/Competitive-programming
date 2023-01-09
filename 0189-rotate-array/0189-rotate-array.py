4b502a6fce8e37ceb2a00a5993966d4734d75a04
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-01-09 05:21:42 -0800

    Time: 233 ms (79.38%), Space: 25.4 MB (78.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,20 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k=k%len(nums)
-        l,r=0,len(nums)-1
+        r=len(nums)-1
+        l=0
         while l<r:
             nums[l],nums[r]=nums[r],nums[l]
             l+=1
             r-=1
-        l,r=0,k-1
+        l=0
+        r=k-1
         while l<r:
             nums[l],nums[r]=nums[r],nums[l]
             l+=1
             r-=1
-        l,r=k,len(nums)-1
+        l=k
+        r=len(nums)-1
         while l<r:
             nums[l],nums[r]=nums[r],nums[l]
             l+=1
             r-=1
-        
+        return nums
         
