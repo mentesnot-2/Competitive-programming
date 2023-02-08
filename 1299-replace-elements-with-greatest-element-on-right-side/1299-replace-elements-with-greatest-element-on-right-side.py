34a3c138f302c3342a36c64aacd14b877d745c3d
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-02-08 11:19:33 -0800

    Time: 754 ms (85.85%), Space: 15.5 MB (37.35%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,18 +1,16 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        index = 0
-        for i in range(len(arr) - 1):
-            
-            if index <= i:
-                flag = arr[i + 1]
-
+        indx=0
+        for i in range(len(arr)-1):
+            if indx<=i:
+                flag=arr[i+1]
                 for j in range(i+1,len(arr)):
                     if arr[j]>flag:
                         flag=arr[j]
-                        index = j
-                arr[i] = flag
+                        indx=j
+                arr[i]=flag
             else:
-                arr[i] = arr[index]
+                arr[i]=arr[indx]
         arr[-1]=-1
         return arr
-        
+                    
