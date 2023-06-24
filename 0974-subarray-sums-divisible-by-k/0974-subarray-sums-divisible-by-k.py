3faf4c615acaf1c703dee0bf0cb74efbf1025fc3
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-06-24 10:04:40 +0000

    Time: 319 ms (32.21%), Space: 21.3 MB (5.50%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,15 +1,15 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         count=0
-        prefix=[0]
-        for i in range(len(nums)):
-            prefix.append(prefix[-1]+nums[i])
+        prefix=[0] + list(itertools.accumulate(nums))
+        
         freq={}
         for i in prefix:
             if i%k in freq:
                 count+=freq[i%k]
-                freq[i%k]+=1
+               
             elif i%k not in freq:
                 freq[i%k]=0
-                freq[i%k]+=1
+            freq[i%k]+=1
         return count
+        
