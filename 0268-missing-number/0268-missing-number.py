5362895dcc16c859b5dfe430076b3140e037703e
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-07-12 07:24:38 +0000

    Time: 131 ms (97.18%), Space: 17.6 MB (82.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        num = sorted(nums)
-        
-        check = {val:indx for val,indx in enumerate(num)}
-        
-        for i in check:
-            if i != check[i]:
-                return i
-        return num[-1] + 1       
-        
+        n = len(nums)
+        return int((n*(n+1)/2) - sum(nums))
