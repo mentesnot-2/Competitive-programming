c905c1ea7b54199f2585357d321b25d23b56f4db
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-08-11 08:08:12 +0000

    Time: 552 ms (65.15%), Space: 17 MB (12.06%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,13 +1,18 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter=Counter(tasks)
-        freq=sorted(list(counter.values()))
-
-        max_idle=freq.pop()
-        total=(max_idle-1)*n
-
-        while freq and total>0:
-            total=total-min(max_idle-1,freq.pop())
-
-        total=max(0,total)
-        return len(tasks) + total
+        count = Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        
+        heapq.heapify(maxHeap)
+        time = 0
+        q = deque()
+        
+        while maxHeap or q:
+            time+=1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt,time + n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap,q.popleft()[0])
+        return time
