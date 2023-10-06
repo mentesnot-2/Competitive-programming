5a3eecd9fe5f6bbcc14e4288fc0c989dbce7e0b3
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-10-06 14:23:58 +0000

    Time: 39 ms (72.47%), Space: 16.3 MB (57.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,9 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        count = Counter(nums)
         
-        for i in range(len(nums) - 1):
-            for j in range(i + 1,len(nums)):
-                if nums[j] < nums[i]:
-                    nums[j],nums[i] = nums[i],nums[j]
-        return nums
+        for i in range(count[0]):
+            nums[i] = 0
+        for i in range(count[0], count[0] + count[1]):
+            nums[i] = 1
+        for i in range(count[0]+ count[1],len(nums)):
+            nums[i] = 2
