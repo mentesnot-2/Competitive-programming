c3a30124af2415db711909390564c92b44f1c210
mentesnot-2 <113641638+mentesnot-2@users.noreply.github.com>
2023-10-21 14:25:42 +0300

    Time: 67 ms (82.36%), Space: 17 MB (67.29%) - LeetHub

---
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -0,0 +1,50 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.k = k
+        self.q = [0] * k
+        self.rear = k - 1
+        self.size = 0
+        self.front = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.front = (self.front - 1 + self.k) % self.k
+        self.q[self.front] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.k
+        self.q[self.rear] = value
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.k) % self.k
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.front]
+
+    def getRear(self) -> int:
+        return -1 if self.isEmpty() else self.q[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
